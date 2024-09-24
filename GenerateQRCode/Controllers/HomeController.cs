ce9668b14a6e4264b6f429e5070a22ba6307ac74
Mirian Adamia <m.adamia@Imedil.ge>
2024-09-24 21:59:45 +0400

    Add project files.

---
+++ GenerateQRCode/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using GenerateQRCode.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenerateQRCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string InputText)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator oQRCodeGenerator = new QRCodeGenerator();
+                QRCodeData oqRCodeData = oQRCodeGenerator.CreateQrCode(InputText, QRCodeGenerator.ECCLevel.Q);
+
+                QRCode oQRCode = new QRCode(oqRCodeData);
+
+                using(Bitmap oBitmap = oQRCode.GetGraphic(20))
+                {
+                    oBitmap.Save(ms,ImageFormat.Png);
+                    ViewBag.QRCode = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+                return View();
+        }
+    }
+}
